c28fed8e55485ec3f4cbe9d37c04aaa0bdba13bb
Zhukov Roman <zhukov.roman@gmail.com>
2022-02-17 01:14:44 +0300

    Breaking changes

--- src/Post.php
+++ src/Post.php
@@ -7,12 +7,15 @@ namespace Example;
 use Cycle\Annotated\Annotation\Entity;
 use Cycle\Annotated\Annotation\Column;
 use Cycle\Annotated\Annotation\Relation;
+use Cycle\ORM\Entity\Behavior\Uuid\Uuid4;
+use Ramsey\Uuid\UuidInterface;
 
 #[Entity]
+#[Uuid4]
 class Post
 {
-    #[Column(type: "integer", primary: true)]
-    private int $id;
+    #[Column(type: "uuid", primary: true)]
+    private UuidInterface $uuid;
 
     #[Relation\HasMany(target: Comment::class)]
     private array $comments = [];
